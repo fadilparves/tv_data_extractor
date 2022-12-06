40d4fa13eb9df57beb90118a04787c0d76fa00a5
fadilparves <fadil.parves@gmail.com>
2022-12-06 15:47:55 +0800

    init the driver working

--- sample_fcpo.py
+++ sample_fcpo.py
@@ -3,8 +3,14 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-os.environ['tv_password']
 
+extractor = TVExtractor(['FCPOF2023', 'FCPOG2023'], os.environ['tv_username'], os.environ['tv_password'], "C:/Users/Mohamad Fadil/Downloads", "JqgggM9t")
+
+driver = None
+while driver == None:
+    driver = extractor.init_driver()
+
+extractor.deinit(driver)
 
 
 
